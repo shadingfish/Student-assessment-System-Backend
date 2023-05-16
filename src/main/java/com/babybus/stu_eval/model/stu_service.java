b1c003172cb612bee80733e2a512c298a6308854
Jori <11627406+wuqilewuqile@user.noreply.gitee.com>
2023-05-16 18:17:01 +0800

    学生竞赛类更新

--- src/main/java/com/babybus/stu_eval/model/stu_service.java
+++ src/main/java/com/babybus/stu_eval/model/stu_service.java
@@ -1,11 +1,13 @@
 package com.babybus.stu_eval.model;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Data
+@ApiModel(description = "记载学生骨干服务情况")
 public class stu_service {
     private String depart;
     private String level;
