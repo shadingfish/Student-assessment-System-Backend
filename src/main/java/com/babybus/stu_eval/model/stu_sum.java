2e10fc3e4fe47e29de4f5276332fdcc2279c3e79
Jori <11627406+wuqilewuqile@user.noreply.gitee.com>
2023-05-16 19:33:27 +0800

    sum更新

---
+++ src/main/java/com/babybus/stu_eval/model/stu_sum.java
@@ -0,0 +1,50 @@
+package com.babybus.stu_eval.model;
+
+import lombok.Data;
+import oracle.jrockit.jfr.StringConstantPool;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Data
+public class stu_sum {
+    private String perf_id;
+    private String stu_id;
+    @Length(min=9,max=9,message = "请输入正确的格式，例如：2022-2023")
+    @Pattern(regexp = "202[0-9]-202[0-9]$", message = "请输入正确的格式，例如：2022-2023")
+    private String ac_year;
+    private String scholarship;
+    @Pattern(regexp = "待审核|已通过|未通过", message = "请输入”待审核“、”已通过“、”未通过“其中一种")
+    private String eval_status;
+    @Max(100)
+    @Min(0)
+    private Integer gpa_score;
+    @Max(100)
+    @Min(0)
+    private Integer report_score;
+    @Max(100)
+    @Min(0)
+    private Integer volun_score;
+    @Max(100)
+    @Min(0)
+    private Integer occup_score;
+    @Max(100)
+    @Min(0)
+    private Integer research_score;
+    @Max(100)
+    @Min(0)
+    private Integer award_score;
+    @Max(100)
+    @Min(0)
+    private Integer practice_score;
+
+
+
+
+
+
+}
