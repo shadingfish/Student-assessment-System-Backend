2799d0301bb63194b91e43201c38dd5205cdda9a
yudingyi <yudingyi054@gmail.com>
2023-06-01 16:43:26 +0800

    修改文件结构

--- src/main/java/com/babybus/stu/eval/yudingyi/config/RedisConfig.java
+++ src/main/java/com/babybus/stu/eval/yudingyi/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.babybus.stu.eval.yudingyi.controller;
+package com.babybus.stu.eval.yudingyi.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
