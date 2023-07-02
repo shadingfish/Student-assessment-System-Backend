2f649e726727eb028fef0309e2217ba281549f27
wuqilewuqile <11627406+wuqilewuqile@user.noreply.gitee.com>
2023-07-02 12:27:37 +0800

    完善竞赛获取页面，增加文件上传功能

--- src/main/java/com/babybus/common/model/material/Competition.java
+++ src/main/java/com/babybus/common/model/material/Competition.java
@@ -1,5 +1,6 @@
 package com.babybus.common.model.material;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,6 +13,7 @@ import java.sql.Date;
 public class Competition extends Material{
     private String compName;
     private String level;
+    @JsonFormat(pattern = "yyyy-MM-dd", locale = "zh", timezone = "GMT+8")
     private Date awardTime;
     private Integer ranking;
     private String award;
